557d9881da5e3a4884784d01a83239e043d28c64
KizahashiLuca <kizahashi.luca.25@gmail.com>
2022-09-13 01:34:37 +0900

    [Updated] beta-2.0.1

--- load_advancements.sh
+++ load_advancements.sh
@@ -8,8 +8,8 @@ function filecomment() {
 ## MC-Version: Java Edit. 1.19
 ## Author    : @potage00
 ## Author    : @KizahashiLuca
-## Date      : 28 Aug 2022
-## Version   : β-2.0
+## Date      : 13 Sep 2022
+## Version   : β-2.0.1
 ## Licensed under CC BY-SA 4.0. 
 #####################################
 
