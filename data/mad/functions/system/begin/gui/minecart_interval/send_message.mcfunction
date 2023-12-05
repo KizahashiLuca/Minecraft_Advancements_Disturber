c95d86c34302d166efea3bb459161ff6862d9690
KizahashiLuca <kizahashi.luca.25@gmail.com>
2023-12-06 08:55:19 +0900

    feat: beta-2.5 date

--- data/mad/functions/system/begin/gui/minecart_interval/send_message.mcfunction
+++ data/mad/functions/system/begin/gui/minecart_interval/send_message.mcfunction
@@ -3,7 +3,7 @@
 ## MC-Version: Java Edit. 1.20.3
 ## Author    : @potagegatop
 ## Author    : @KizahashiLuca
-## Date      : 19 Nov 2023
+## Date      : 06 Dec 2023
 ## Version   : β-2.5
 ## Licensed under CC BY-SA 4.0. 
 #####################################
--- load_advancements.sh
+++ load_advancements.sh
@@ -8,7 +8,7 @@ function filecomment() {
 ## MC-Version: Java Edit. 1.20.3
 ## Author    : @potagegatop
 ## Author    : @KizahashiLuca
-## Date      : 19 Nov 2023
+## Date      : 06 Dec 2023
 ## Version   : β-2.5
 ## Licensed under CC BY-SA 4.0. 
 #####################################
