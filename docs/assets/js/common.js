6206a64f8c75a3f2470b940cdefcee100d0fee5f
KizahashiLuca <kizahashi.luca.25@gmail.com>
2024-04-11 22:25:38 +0900

    fix: path include

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -3,9 +3,9 @@ const headerContent = document.getElementById('headerContent');
 const footerContent = document.getElementById('footerContent');
 
 // Fetch header and footer
-fetch('/include/header.html')
+fetch('/Minecraft_Advancements_Disturber/include/header.html')
   .then((response) => response.text())
   .then((data) => headerContent.innerHTML = data);
-fetch('/include/footer.html')
+fetch('/Minecraft_Advancements_Disturber/include/footer.html')
   .then((response) => response.text())
   .then((data) => footerContent.innerHTML = data);
